8012fcecd004fce33c351acca251f2ac5bfb3f4d
Dmitrii <nikidim2017@gmail.com>
2023-04-19 13:33:49 +0200

    Added day 3 task 1 (добавлен цикл, условие со словом "Stop/stop", добавлена переменная country.

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -6,35 +6,46 @@ public class Task1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String cityName = sc.next();
 
-        switch (cityName) {
-            case "Москва":
-            case "Владивосток":
-            case "Ростов":
-                System.out.println("Россия");
+        while (true) {
+            String cityName = sc.next();
+            if (!cityName.equalsIgnoreCase("stop")) {
+
+                String country = "";
+                switch (cityName) {
+
+                    case "Москва":
+                    case "Владивосток":
+                    case "Ростов":
+                        country = "Россия";
+                        break;
+
+                    case "Рим":
+                    case "Милан":
+                    case "Турин":
+                        country = "Италия";
+                        break;
+
+                    case "Ливерпуль":
+                    case "Манчестер":
+                    case "Лондон":
+                        country = "Англия";
+                        break;
+
+                    case "Берлин":
+                    case "Мюнхен":
+                    case "Кёльн":
+                        country = "Германия";
+                        break;
+
+                    default:
+                        System.out.println("Неизвестная страна");
+                }
+                System.out.println(country);
+            } else {
+                System.out.println("Введено слово stop, программа завершила работу");
                 break;
-
-            case "Рим":
-            case "Милан":
-            case "Турин":
-                System.out.println("Италия");
-                break;
-
-            case "Ливерпуль":
-            case "Манчестер":
-            case "Лондон":
-                System.out.println("Англия");
-                break;
-
-            case "Берлин":
-            case "Мюнхен":
-            case "Кёльн":
-                System.out.println("Германия");
-                break;
-
-            default:
-                System.out.println("Неизвестная страна");
+            }
         }
         sc.close();
     }
